cdb07bb6bad9ff7507e7efee822b043f0b2edf19
MH35JP <mh35jp@gmail.com>
2023-05-11 09:40:38 +0900

    Initial commit

---
+++ netlify/functions/hello.ts
@@ -0,0 +1,37 @@
+import { Handler } from '@netlify/functions'
+
+export const handler: Handler = async (event, context) => {
+  const url = process.env.API_URL;
+  if (url === undefined) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({result: "API URL is not set"})
+    };
+  }
+  const data = event.body;
+  if (data === null) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({reult: "No body provided"})
+    };
+  }
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      body: data,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const rData = await resp.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(rData)
+    };
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({result: "API failed"})
+    };
+  }
+}
